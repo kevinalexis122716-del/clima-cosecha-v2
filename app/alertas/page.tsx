3921166fe17c2906ddfd589d597a638c536c55e6
kevinalexis122716-del <kevinalexis122716@gmail.com>
2026-05-24 16:48:49 -0500

    fix paginas vacias

---
+++ app/alertas/page.tsx
@@ -0,0 +1,7 @@
+export default function Alertas() {
+  return (
+    <div>
+      <h1>Alertas</h1>
+    </div>
+  )
+}
---
+++ app/pronostico/page.tsx
@@ -0,0 +1,7 @@
+export default function Pronosticos() {
+  return (
+    <div>
+      <h1>Pronósticos</h1>
+    </div>
+  )
+}
